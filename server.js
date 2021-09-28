1448c3c1bc2772effbf0041074a1f4651a29a7fd
jalbiston <jacob.albiston@gmail.com>
2021-09-28 14:18:12 -0600

    gaaaah

--- server.js
+++ server.js
@@ -6,14 +6,22 @@ app.use(express.json())
 
 const Rollbar =require('rollbar')
 
+const rollbar = new Rollbar({
+    accesToken: '',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
 
 app.use(express.static("client"));
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './client/index.html'))
-//     rollbar.info('html was monitored successfully!')
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/index.html'))
+    rollbar.info('html was monitored successfully!')
+})
 
 
 const port = process.env.PORT || 5656
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => console.log(`Hippity hoppity your server is on port: ${port}`))
