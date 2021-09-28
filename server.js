6c08fb063e9d39384e938174e8f384616758f39c
jalbiston <jacob.albiston@gmail.com>
2021-09-28 14:07:59 -0600

    added app.get

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const Rollbar =require('rollbar')
 
 
 
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/index.html'))
+    rollbar.info('html was monitored successfully!')
+})
 
 
 const port = process.env.PORT || 5656
